f681bc5740ce86c76123ab7137b9a13c51783480
Nando2119 <56266050+Nando2119@users.noreply.github.com>
2020-03-25 21:42:02 +0000

    login function created still needs implementation with the thread

---
+++ FarmWeatherClientServer/src/farmweatherclientserver/LoginCheck.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package farmweatherclientserver;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+/**
+ *
+ * @author Fernando
+ */
+public class LoginCheck {
+
+    private static Scanner newUser;
+
+    Boolean userLogin() {
+    File filePath = new File("C:\\Users\\Fernando\\Documents\\Year 2\\Systems Software\\FarmWeatherClientServer\\src\\farmweatherclientserver\\Users.txt");
+    
+    Scanner userDetails = new Scanner(System.in);
+    
+    System.out.println("Enter your Usernamer:");
+    String username = userDetails.nextLine();
+    
+    System.out.println("Enter your Password:");
+    String password = userDetails.nextLine();
+    
+    return userVerify(username, password, filePath);
+    }
+    
+    public static Boolean userVerify(String username, String password, File filePath){
+        String checkUsername;
+        String checkPassword;
+        Boolean isCorrect = false;
+        
+        try{
+        
+        newUser = new Scanner(filePath);
+        newUser.useDelimiter(",|\\n");
+        
+        while(!isCorrect && newUser.hasNext())
+        {
+            checkUsername = newUser.next();
+            checkPassword = newUser.next();
+            
+            if(checkUsername.trim().equals(username) && checkPassword.trim().equals(password))
+            {
+                isCorrect = true;
+            }
+            
+        }
+        newUser.close();
+        
+        }
+        catch(FileNotFoundException e)
+        {
+            System.out.println("Something went wrong :/");
+        }
+        return isCorrect;
+    }
+}
